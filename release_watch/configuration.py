3b231af7e1282115ea1d2a7e769d4746176831b1
Anuj Patel <anujp31@gmail.com>
2022-06-15 10:33:03 -0400

    better exception handling

--- release_watch/configuration.py
+++ release_watch/configuration.py
@@ -4,15 +4,15 @@ from os.path import isfile, expanduser
 
 class Config:
     def __init__(self):
-        self.config_file_paths = ["~/.config/release-watch/config.toml"]
+        self.config_file_paths = ["~/.config/release-watch.toml"]
         self.conf = self.load_config()
 
     def load_config(self):
         for path in self.config_file_paths:
             p = expanduser(path)
-            if isfile(p):
+            try:
                 with open(p, "r") as f:
                     c = toml.load(f)
                 return c
-        else:
-            raise Exception("No config file found")
+            except FileNotFoundError as e:
+                raise e
